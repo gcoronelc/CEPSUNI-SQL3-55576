168935c7691e06e05f2d265309d6375f69081f2b
Gustavo Coronel <gcoronelc@gmail.com>
2021-05-02 12:09:02 -0500

    Clase 05

--- Clase05/SQLQuery1.sql
+++ Clase05/SQLQuery1.sql
@@ -38,4 +38,72 @@ go
 
 
 
+select * 
+into Curso5
+from Curso
+where 1=2;
+go
+
+
+select * from Curso5;
+go
+
+
+BULK INSERT dbo.Curso5
+FROM 'D:\bcp\Curso-nat.Dat' 
+WITH (DATAFILETYPE='native'); 
+GO
+
+select * from Curso5;
+go
+
+
+
+select * into Curso6
+from Curso where 1=2;
+go
+
+INSERT INTO dbo.Curso6 
+SELECT a.* 
+FROM OPENROWSET( BULK 'D:\bcp\Curso_A.dat', FORMATFILE = 'D:\bcp\Curso.fmt') AS a;
+go
+
+select * from Curso6;
+go
+
+
+-- CONSULTAS DISTRIBUIDAS
+-- ======================
+
+
+sp_configure 'show advanced options', 1; 
+go
+
+
+RECONFIGURE; 
+go
+
+
+sp_configure 'Ad Hoc Distributed Queries', 1; 
+go
+
+
+RECONFIGURE;
+go
+
+
+SELECT a.* 
+FROM OPENROWSET('SQLNCLI', 
+		'Server=localhost;
+		Trusted_Connection=yes;', 
+		'SELECT * FROM EDUTEC.dbo.curso') AS a;
+go
+
+
+SELECT a.* 
+FROM OPENROWSET('SQLNCLI', 
+		'Server=localhost;userid=sa;password=sql', 
+		'SELECT * FROM EDUTEC.dbo.curso') AS a;
+go
+
 
