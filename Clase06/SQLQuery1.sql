0dd9940d69d9d25a096a0e467bec1e8facb3f64d
Gustavo Coronel <gcoronelc@gmail.com>
2021-05-16 16:59:07 -0500

    Clase 6

---
+++ Clase06/SQLQuery1.sql
@@ -0,0 +1,17 @@
+
+SELECT * FROM DBO.TIEMPO;
+GO
+
+select CustomerID, CompanyName, ContactName, Phone
+from dbo.Customers
+
+
+SELECT 
+	YEAR(O.OrderDate) Anio, MONTH(O.OrderDate) Mes,
+	C.CustomerID, C.CompanyName, O.OrderID, 
+	CONVERT(varchar,O.OrderDate,103) OrderDate,
+	P.ProductName, D.UnitPrice, D.Quantity
+FROM Northwind.dbo.Customers AS C
+JOIN Northwind.dbo.Orders AS O ON C.CustomerID = O.CustomerID
+JOIN Northwind.dbo.[Order Details] AS D ON O.OrderID = D.OrderID
+JOIN Northwind.dbo.Products AS P ON D.ProductID = P.ProductID;
